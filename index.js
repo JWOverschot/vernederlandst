6c5486afaa1ef34f2b1d0fb9fda050d8c25dc773
JWOverschot <99040091@mydavinci.nl>
2017-06-16 10:03:19 +0200

    basic layout

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.listen(3000, function() {
   console.log('listening on 3000')
 })
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/public/view/home/index.html')
 })
 
 app.post('/quotes', (req, res) => {
